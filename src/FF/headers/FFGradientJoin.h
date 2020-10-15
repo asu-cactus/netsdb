f97c1a4d0621ae1cdb5f96e652c4a96f50315dce
Amitabh Das <amitab.das@outlook.com>
2020-10-15 00:53:54 -0700

    Fix gradient join

--- src/FF/headers/FFGradientJoin.h
+++ src/FF/headers/FFGradientJoin.h
@@ -28,27 +28,28 @@ public:
   getProjection(Handle<FFMatrixBlock> in1, Handle<FFMatrixBlock> in2) override {
     return makeLambda(
         in1, in2, [](Handle<FFMatrixBlock> &in1, Handle<FFMatrixBlock> &in2) {
-          int rowNums = in1->getRowNums();
-          int colNums = in2->getColNums();
-          int blockRowIndex = in1->getBlockRowIndex();
-          int blockColIndex = in2->getBlockColIndex();
-          int totalRows = in1->getTotalRowNums();
-          int totalCols = in2->getTotalColNums();
+          // get the sizes
+          uint32_t I = in1->getRowNums();
+          uint32_t K = in1->getColNums();
 
+          std::cout << "Input matrices of size : " << I << " x " << K << " and " << in2->getRowNums() << " x " << in2->getColNums() << " --<GradientJoin>---> " << I << " x " << K << std::endl;
+
+          // make an output
           pdb::Handle<FFMatrixBlock> resultFFMatrixBlock =
-              pdb::makeObject<FFMatrixBlock>(blockRowIndex, blockColIndex,
-                                             rowNums, colNums, totalRows,
-                                             totalCols);
+              pdb::makeObject<FFMatrixBlock>(
+                  in1->getBlockRowIndex(), in1->getBlockColIndex(), I, K,
+                  in1->getTotalRowNums(), in1->getTotalColNums());
 
           auto data = resultFFMatrixBlock->getValue().rawData->c_ptr();
           auto lhs = in1->getValue().rawData->c_ptr();
           auto rhs = in2->getValue().rawData->c_ptr();
 
           // do the multiply
-          for (int32_t i = 0; i < in1->getRowNums() * in1->getColNums(); i++) {
-            data[i] = lhs[i] * (rhs[i] > 0) * 1.0;
+          for (int32_t i = 0; i < I * K; i++) {
+            data[i] = lhs[i] * (rhs[i] > 0) * 1.0f;
           }
 
+          // return the output
           return resultFFMatrixBlock;
         });
   }
