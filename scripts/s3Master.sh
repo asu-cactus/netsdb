14e6ac724f1f6e82283aff309425fc4c05177b85
PratikBarhate <pratikbarhate00@outlook.com>
2021-01-25 18:32:11 -0700

    S3 bucket name passed as parameter.

--- scripts/s3Master.sh
+++ scripts/s3Master.sh
@@ -2,13 +2,15 @@
 
 num_of_arg=$#
 
-if [ ${num_of_arg} -eq 2 ]; then
+if [ ${num_of_arg} -eq 3 ]; then
   echo "Private key given is ${1}"
   echo "Task to execute is ${2}"
+  echo "AWS S3 bucket name is ${3}"
 else
   echo "Two argument is expected"
   echo "1. Private key to SSH into the worker machines."
   echo "2. Task to execute - 'flush' or 'load'"
+  echo "3. AWS S3 bucket name"
   exit 1
 fi
 
@@ -16,7 +18,7 @@ workers=($(awk '{print $0}' $PDB_HOME/conf/serverlist))
 echo "There are ${#workers[@]} worker nodes"
 
 for w in ${workers[@]}; do
-  ssh -i ${1} ${w} "bash $PDB_INSTALL/scripts/s3Worker.sh ${2} ${PDB_INSTALL}" &
+  ssh -i ${1} ${w} "bash $PDB_INSTALL/scripts/s3Worker.sh ${2} ${PDB_INSTALL} ${3}" &
   echo "Process started for ${w} worker with process id ${!}"
 done
 
--- scripts/s3Worker.sh
+++ scripts/s3Worker.sh
@@ -2,26 +2,27 @@
 
 num_of_arg=$#
 
-if [ ${num_of_arg} -eq 2 ]; then
+if [ ${num_of_arg} -eq 3 ]; then
   echo "Task to execute is ${1}"
   echo "Install Directory is ${2}"
+  echo "AWS S3 bucket name is ${3}"
 else
   echo "Two arguments are expected"
   echo "1. Task to execute - 'flush' or 'load'"
   echo "2. PDB Install Directory"
+  echo "3. AWS S3 bucket name"
   exit 1
 fi
 
 self_ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-echo "PDB install directory is ${PDB_INSTALL}"
 
 if [ "${1}" == "flush" ]; then
   aws s3 cp --region us-east-2 --recursive \
   ${2}/pdbRoot_localhost_8108/  \
-  s3://lachesisstore/worker_data/"${self_ip}"/pdbRoot_localhost_8108/
+  ${3}/worker_data/"${self_ip}"/pdbRoot_localhost_8108/
 elif [ "${1}" == "load" ]; then
   aws s3 cp --region us-east-2 --recursive \
-  s3://lachesisstore/worker_data/"${self_ip}"/pdbRoot_localhost_8108/ \
+  ${3}/worker_data/"${self_ip}"/pdbRoot_localhost_8108/ \
   ${2}/pdbRoot_localhost_8108/
 fi
 
