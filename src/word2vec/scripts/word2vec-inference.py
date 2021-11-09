0133a009cca10799e684c56c45e0e37d33584b55
jiazou-bigdata <jacquelinezou@gmail.com>
2021-11-09 23:44:32 +0000

    More changes to the inference script.

--- src/word2vec/scripts/word2vec-inference.py
+++ src/word2vec/scripts/word2vec-inference.py
@@ -2,144 +2,147 @@ import io
 import re
 import string
 import tqdm
+import time
 
 import numpy as np
-
+import os
 import tensorflow as tf
 from tensorflow.keras import layers
 import tensorflow_hub as hub
 from tensorflow.keras.models import Sequential
 from keras.initializers import Constant
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 SEED = 42
 AUTOTUNE = tf.data.AUTOTUNE
 
-class Word2Vec(tf.keras.Model):
-  def __init__(self, vocab_size, embedding_dim):
-    super(Word2Vec, self).__init__()
-    #self.target_embedding = hub.KerasLayer("https://tfhub.dev/google/Wiki-words-500/2", trainable=True, input_shape=[], dtype=tf.string)
-    #self.context_embedding = hub.KerasLayer("https://tfhub.dev/google/Wiki-words-500/2", trainable=True, input_shape=[], dtype=tf.string)
-    self.target_embedding = layers.Embedding(vocab_size,
-                                      embedding_dim,
-                                      input_length=1,
-                                      name="w2v_embedding")
-  def get_layer(self):
-      return self.target_embedding
-
-  def call(self, target):
-    if len(target.shape) == 2:
-      target = tf.squeeze(target, axis=1)
-    word_emb = self.target_embedding(target)
-    return word_emb
-
-# Generates skip-gram pairs with negative sampling for a list of sequences
-# (int-encoded sentences) based on window size, number of negative samples
-# and vocabulary size.
-def generate_training_data(sequences, window_size, num_ns, vocab_size, seed):
-  # Elements of each training example are appended to these lists.
-  targets, contexts, labels = [], [], []
-
-  # Build the sampling table for vocab_size tokens.
-  sampling_table = tf.keras.preprocessing.sequence.make_sampling_table(vocab_size)
-
-  # Iterate over all sequences (sentences) in dataset.
-  for sequence in tqdm.tqdm(sequences):
-
-    # Generate positive skip-gram pairs for a sequence (sentence).
-    positive_skip_grams, _ = tf.keras.preprocessing.sequence.skipgrams(
-          sequence,
-          vocabulary_size=vocab_size,
-          sampling_table=sampling_table,
-          window_size=window_size,
-          negative_samples=0)
-
-
-    # Iterate over each positive skip-gram pair to produce training examples
-    # with positive context word and negative samples.
-    for target_word, context_word in positive_skip_grams:
-      context_class = tf.expand_dims(
-          tf.constant([context_word], dtype="int64"), 1)
-      negative_sampling_candidates, _, _ = tf.random.log_uniform_candidate_sampler(
-          true_classes=context_class,
-          num_true=1,
-          num_sampled=num_ns,
-          unique=True,
-          range_max=vocab_size,
-          seed=SEED,
-          name="negative_sampling")
-
-      # Build context and label vectors (for one target word)
-      negative_sampling_candidates = tf.expand_dims(
-          negative_sampling_candidates, 1)
-
-      context = tf.concat([context_class, negative_sampling_candidates], 0)
-      label = tf.constant([1] + [0]*num_ns, dtype="int64")
-
-      # Append each element from the training example to global lists.
-      targets.append(target_word)
-      contexts.append(context)
-      labels.append(label)
-
-  return targets, contexts, labels
-
-
-# Now, create a custom standardization function to lowercase the text and
-# remove punctuation.
-def custom_standardization(input_data):
-  lowercase = tf.strings.lower(input_data)
-  return tf.strings.regex_replace(lowercase,
-                                  '[%s]' % re.escape(string.punctuation), '')
-
-
 if __name__ == "__main__":
-
-    path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
-    with open(path_to_file) as f:
-        lines = f.read().splitlines()
-    text_ds = tf.data.TextLineDataset(path_to_file).filter(lambda x: tf.cast(tf.strings.length(x), bool))
 
     # Define the vocabulary size and number of words in a sequence.
     vocab_size = 1009375
     embedding_dim = 500
-    sequence_length = 10
-    num_ns = 4
 
-    # Use the text vectorization layer to normalize, split, and map strings to
-    # integers. Set output_sequence_length length to pad all samples to same length.
-    vectorize_layer = layers.experimental.preprocessing.TextVectorization(
-        standardize=custom_standardization,
-        max_tokens=vocab_size,
-        output_mode='int',
-        output_sequence_length=sequence_length,
-        vocabulary="/home/ubuntu/saved_model/assets/tokens.txt")
+    start = time.time()
 
-    #vectorize_layer.adapt(text_ds.batch(1024))
-    text_vector_ds = text_ds.batch(1024).prefetch(AUTOTUNE).map(vectorize_layer).unbatch()
-    sequences = list(text_vector_ds.as_numpy_iterator())
+    print("loading model-1")
 
+    targets1 = np.random.randint(0, vocab_size-1, (100,))
+    #print(targets1)
+    #print(targets1.shape)
 
-    targets, contexts, labels = generate_training_data(
-        sequences=sequences,
-        window_size=2,
-        num_ns=4,
-        vocab_size=vocab_size,
-        seed=SEED)
-
-    targets = np.array(targets)
-    contexts = np.array(contexts)[:,:,0]
-    labels = np.array(labels)
-
-    BATCH_SIZE = 100
-    BUFFER_SIZE = 10000
-    dataset = tf.data.Dataset.from_tensor_slices(((targets, contexts), labels))
-    dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-    dataset = dataset.cache().prefetch(buffer_size=AUTOTUNE)
+    #dataset = tf.data.Dataset.from_tensor_slices(((targets, contexts), labels))
+    #dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+    #dataset = dataset.cache().prefetch(buffer_size=AUTOTUNE)
+    #print(dataset)
 
     word2vec = tf.keras.models.load_model("word2vec")
     layer2 = word2vec.get_layer("w2v_embedding")
+    weights1= layer2.get_weights()
     word2vec1 = Sequential()
-    embedding = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(layer2.get_weights()), trainable=False)
-    word2vec1.add(embedding)
-    results = word2vec1.predict(dataset)
-    print(results)
+    embedding1 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights1), trainable=False)
+    word2vec1.add(embedding1)
 
+    print("loading model-2")
+
+    targets2 = np.random.randint(0, vocab_size-1, (100,))
+    weights2 = layer2.get_weights()
+    word2vec2 = Sequential()
+    embedding2 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights2), trainable=False)
+    word2vec2.add(embedding2)
+
+
+    print("loading model-3")
+
+    targets3 = np.random.randint(0, vocab_size-1, (100,))
+    weights3 = layer2.get_weights()
+    word2vec3 = Sequential()
+    embedding3 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights3), trainable=False)
+    word2vec3.add(embedding3)
+
+    print("loading model-4")
+
+    targets4 = np.random.randint(0, vocab_size-1, (100,))
+    weights4 = layer2.get_weights()
+    word2vec4 = Sequential()
+    embedding4 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights4), trainable=False)
+    word2vec4.add(embedding4)
+
+    print("loading model-5")
+
+    targets5 = np.random.randint(0, vocab_size-1, (100,))
+    weights5 = layer2.get_weights()
+    word2vec5 = Sequential()
+    embedding5 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights5), trainable=False)
+    word2vec5.add(embedding5)
+
+
+#    print("loading model-6")
+
+#    targets6 = np.random.randint(0, vocab_size-1, (100,))
+#    weights6 = layer2.get_weights()
+#    word2vec6 = Sequential()
+#    embedding6 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights6), trainable=False)
+#    word2vec6.add(embedding5)
+
+
+#    print("loading model-7")
+
+#    targets7 = np.random.randint(0, vocab_size-1, (100,))
+#    weights7 = layer2.get_weights()
+#    word2vec7 = Sequential()
+#    embedding7 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights7), trainable=False)
+#    word2vec7.add(embedding2)
+
+
+#    print("loading model-8")
+
+#    targets8 = np.random.randint(0, vocab_size-1, (100,))
+#    weights8 = layer2.get_weights()
+#    word2vec8 = Sequential()
+#    embedding8 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights8), trainable=False)
+#    word2vec8.add(embedding3)
+
+#    print("loading model-9")
+
+#    targets9 = np.random.randint(0, vocab_size-1, (100,))
+#    weights9 = layer2.get_weights()
+#    word2vec9 = Sequential()
+#    embedding9 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights9), trainable=False)
+#    word2vec9.add(embedding4)
+
+#    print("loading model-10")
+
+#    targets10 = np.random.randint(0, vocab_size-1, (100,))
+#    weights10 = layer2.get_weights()
+#    word2vec10 = Sequential()
+#    embedding10 = layers.Embedding(vocab_size, embedding_dim, embeddings_initializer=Constant(weights10), trainable=False)
+#    word2vec10.add(embedding5)
+
+
+
+
+    inference_start = time.time()
+    results1 = word2vec1.predict(targets1)
+    results2 = word2vec2.predict(targets2)
+    results3 = word2vec3.predict(targets3)
+    results4 = word2vec4.predict(targets4)
+    results5 = word2vec5.predict(targets5)
+#    results6 = word2vec6.predict(targets6)
+#    results7 = word2vec7.predict(targets7)
+#    results8 = word2vec8.predict(targets8)
+#    results9 = word2vec9.predict(targets9)
+#    results10 = word2vec10.predict(targets10)
+    inference_end = time.time()
+    print(results1)
+    print(results2)
+    print(results3)
+    print(results4)
+    print(results5)
+#    print(results6)
+#    print(results7)
+#    print(results8)
+#    print(results9)
+#    print(results10)
+    print('model and data preparation time', inference_start-start)
+    print('inference time for word2vec', inference_end-inference_start)
