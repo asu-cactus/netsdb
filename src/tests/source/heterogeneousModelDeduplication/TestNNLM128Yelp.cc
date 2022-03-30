0fbc6290bd760db5534520aa10dc547ff810b63a
lixizhou <lixi.zhou@asu.edu>
2022-03-30 09:14:35 -0700

    init commit for hetero models

---
+++ src/tests/source/heterogeneousModelDeduplication/TestNNLM128Yelp.cc
@@ -0,0 +1,42 @@
+
+#ifndef TEST_NNLM128_YELP_CC
+#define TEST_NNLM128_YELP_CC
+
+#include "PDBString.h"
+#include "PDBMap.h"
+#include "DataTypes.h"
+#include "TensorBlockIndex.h"
+#include "InterfaceFunctions.h"
+#include "PDBClient.h"
+#include "FFMatrixBlock.h"
+#include "FFMatrixUtil.h"
+#include "SimpleFF.h"
+#include "FFMatrixBlockScanner.h"
+#include "FFMatrixWriter.h"
+#include "FFAggMatrix.h"
+#include "FFTransposeMult.h"
+#include "SemanticClassifier.h"
+#include <cstddef>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <cstring>
+#include <ctime>
+#include <chrono>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string>
+
+using namespace pdb;
+
+
+int main(int argc, char* argv[]) {
+
+     
+}
+
+#endif
