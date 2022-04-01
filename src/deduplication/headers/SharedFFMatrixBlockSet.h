60dbdf8912498935ae0875cb1850f547fb1e687b
jiazou-bigdata <jacquelinezou@gmail.com>
2022-04-01 02:06:08 +0000

    Add shared set and block index that use FFMatrixBlock and FFMatrixMeta.

---
+++ src/deduplication/headers/SharedFFMatrixBlockSet.h
@@ -0,0 +1,117 @@
+
+/*
+ * SharedFFMatrixBlockSet is a special type of set that stores the shared tensor blocks.
+ */
+
+
+#ifndef SHARED_FF_MATRIX_BLOCK_SET_H
+#define SHARED_FF_MATRIX_BLOCK_SET_H
+
+#include "PageCache.h"
+#include "PDBLogger.h"
+#include "DataTypes.h"
+#include "UserSet.h"
+#include "FFMatrixMeta.h"
+#include "FFMatrixBlockIndex.h"
+#include <memory>
+#include <string>
+using namespace std;
+
+namespace pdb{
+
+class SharedFFMatrixBlockSet;
+typedef shared_ptr<SharedFFMatrixBlockSet> SharedFFMatrixBlockSetPtr;
+
+class SharedFFMatrixBlockSet : public UserSet {
+public:
+
+
+    SharedFFMatrixBlockSet(size_t pageSize, pdb::PDBLoggerPtr logger,
+            SharedMemPtr shm,
+            NodeID nodeId,
+            DatabaseID dbId,
+            UserTypeID typeId,
+            SetID setId,
+            string setName,
+	    PartitionedFilePtr file,
+            PageCachePtr pageCache,
+            LocalityType localityType = JobData,
+            LocalitySetReplacementPolicy policy = LRU,
+            OperationType operation = Read,
+            DurabilityType durability = TryCache,
+            PersistenceType persistence = Persistent,
+            size_t desiredSize = 1): UserSet(pageSize, logger, shm, nodeId, dbId, typeId, setId, setName,
+		       file, pageCache, localityType, policy, operation, durability,
+		       persistence, desiredSize, true) {
+		    std::cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << std::endl;
+		    std::cout << "Created shared set:" << setName << std::endl;
+		    if (isShared) {
+		       std::cout << "isShared set to true" << std::endl;
+		    }
+		    std::cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << std::endl;
+    }
+
+    SharedFFMatrixBlockSet(size_t pageSize,
+            pdb::PDBLoggerPtr logger,
+            SharedMemPtr shm,
+            NodeID nodeId,
+            DatabaseID dbId,
+            UserTypeID typeId,
+            SetID setId,
+            string setName,
+            PartitionedFilePtr file,
+	    std::string pathToIndexFile,
+            PageCachePtr pageCache,
+            LocalityType localityType = JobData,
+            LocalitySetReplacementPolicy policy = LRU,
+            OperationType operation = Read,
+            DurabilityType durability = TryCache,
+            PersistenceType persistence = Persistent,
+            size_t desiredSize = 1) : UserSet(pageSize, logger, shm, nodeId, dbId, typeId, setId, setName,
+		       file, pageCache, localityType, policy, operation, durability, 
+		       persistence, desiredSize, true) {
+       indexes.deserializeIndex(pathToIndexFile);
+                           std::cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << std::endl;
+                    std::cout << "Created shared set:" << setName << std::endl;
+                    if (isShared) {
+                       std::cout << "isShared set to true" << std::endl;
+                    }
+                    std::cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << std::endl;
+
+	    }
+
+
+
+    void serializeIndex (std::string path) {
+        indexes.serializeIndex(path);
+    }
+
+    void deserializeIndex(std::string path) {
+        indexes.deserializeIndex(path);
+    }
+
+    Handle<FFMatrixMeta> getTargetMetadata (DatabaseID dbId, UserTypeID typeId, SetID setId, FFMatrixMeta sourceBlockMeta) {
+        return indexes.getTargetMetadata(dbId, typeId, setId, sourceBlockMeta);
+    }
+
+   
+
+
+    bool insertIndex(DatabaseID dbId, UserTypeID typeId, SetID setId, FFMatrixMeta sourceBlockMeta, Handle<FFMatrixMeta> targetBlockMeta) {
+	size_t key = indexes.getSetKey(dbId, typeId, setId);
+	return indexes.insertIndex(key, sourceBlockMeta, targetBlockMeta);
+    }
+
+    bool removeIndex(DatabaseID dbId, UserTypeID typeId, SetID setId, FFMatrixMeta sourceBlockMeta) {
+        size_t key = indexes.getSetKey(dbId, typeId, setId);
+	return indexes.removeIndex(key, sourceBlockMeta);
+    }
+
+private:
+    FFMatrixBlockIndex indexes;
+
+};
+
+}
+
+#endif
