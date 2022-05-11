7aad3ee096dc39eb95284f00ac0ef49e2982de71
Venkatesh Gunda <venkateshgunda1997@gmail.com>
2022-05-11 10:59:29 -0700

    Aggregator Function for XGBoost Output

--- src/decisionTree/headers/Forest.h
+++ src/decisionTree/headers/Forest.h
@@ -78,23 +78,22 @@ namespace decisiontree {
                 return pair1.second < pair2.second;})->first;
         }
 
-        // Decision of an XGBoost Tree (for class-1, not class-0): sigmoid(log(previous_tree_pred(initial_value=0)/(1-previous_tree_pred(initial_value=0))) + learning_rate*(val2/(val1+val2)))
+        // Decision of an XGBoost Tree (for class-1, not class-0): sigmoid(log(previous_tree_pred(initial_value=0)/(1-previous_tree_pred(initial_value=0))) + learning_rate*(current_tree_prob))
         static template<class InputIt, class T = typename std::iterator_traits<InputIt>::value_type> T aggregate_decisions(InputIt begin, InputIt end){
 //          std::map<T, int> counts; // TODO: This function code needs to be re-written for XGBoost
             // Default LR Value Source: https://xgboost.readthedocs.io/en/stable/parameter.html?highlight=0.3#parameters-for-tree-booster
             double learning_rate = 0.3; // TODO: Hard-coding XGBoost Library default value. Change this to a parameter if modified.
+            double aggregated_decision = 0.0; // Initial Prediction is 0.5, which makes log(odds) value 0. If different, the default value changes.
+            double threshold = 0.5;
             for (InputIt it = begin; it != end; ++it) {
-                if (counts.find(*it) != counts.end()) {
-                    ++counts[*it];
-                }
-                else {
-                    counts[*it] = 1;
-                }
+                aggregated_decision += (*it); // Should not be multiplied by Learning Rate
             }
-            return std::max_element(counts.begin(), counts.end(),[] (const std::pair<T, int>& pair1, const std::pair<T, int>& pair2) {
-                return pair1.second < pair2.second;})->first;
+
+            double sigmoid_of_decision = 1/(1+exp(-1.0*aggregated_decision)); // Sigmoid of the aggregated decision is the final output
+            return sigmoid_of_decision > threshold ? 2.0 : 1.0; // Delaying Class Assignment. Class Labels as per RF Code is 1.0 and 2.0
         }
 
+        // TODO: Call this function instead of Individual Compute Functions. Instantiate the Class with the Model Type
         template<class InputIt, class T = typename std::iterator_traits<InputIt>::value_type> T compute_result(InputIt begin, InputIt end) {
             switch(modelType) {
                 case ModelType::RandomForest:
