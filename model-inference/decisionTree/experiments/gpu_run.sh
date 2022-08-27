2e11fa9ea9ad023676750a965298490c48f6fbf6
Mahidhar <mahidhar96@gmail.com>
2022-08-26 23:49:54 -0700

    commenting tvm

--- model-inference/decisionTree/experiments/gpu_run.sh
+++ model-inference/decisionTree/experiments/gpu_run.sh
@@ -7,6 +7,19 @@ python test_model.py -d higgs -m xgboost -f HummingbirdTorchScriptGPU --batch_si
 python test_model.py -d higgs -m xgboost -f ONNXGPU --batch_size 100 --query_size 100
 python test_model.py -d higgs -m xgboost -f ONNXGPU --batch_size 10000 --query_size 10000
 python test_model.py -d higgs -m xgboost -f ONNXGPU --batch_size 1000000 --query_size 1000000
-python test_model.py -d higgs -m xgboost -f HummingbirdTVMGPU --batch_size 100 --query_size 100
-python test_model.py -d higgs -m xgboost -f HummingbirdTVMGPU --batch_size 10000 --query_size 10000
-python test_model.py -d higgs -m xgboost -f HummingbirdTVMGPU --batch_size 1000000 --query_size 1000000
+# python test_model.py -d higgs -m xgboost -f HummingbirdTVMGPU --batch_size 100 --query_size 100
+# python test_model.py -d higgs -m xgboost -f HummingbirdTVMGPU --batch_size 10000 --query_size 10000
+# python test_model.py -d higgs -m xgboost -f HummingbirdTVMGPU --batch_size 1000000 --query_size 1000000
+
+python test_model.py -d higgs -m randomforest -f HummingbirdPytorchGPU --batch_size 100 --query_size 100
+python test_model.py -d higgs -m randomforest -f HummingbirdPytorchGPU --batch_size 10000 --query_size 10000
+python test_model.py -d higgs -m randomforest -f HummingbirdPytorchGPU --batch_size 1000000 --query_size 1000000
+python test_model.py -d higgs -m randomforest -f HummingbirdTorchScriptGPU --batch_size 100 --query_size 100
+python test_model.py -d higgs -m randomforest -f HummingbirdTorchScriptGPU --batch_size 10000 --query_size 10000
+python test_model.py -d higgs -m randomforest -f HummingbirdTorchScriptGPU --batch_size 1000000 --query_size 1000000
+python test_model.py -d higgs -m randomforest -f ONNXGPU --batch_size 100 --query_size 100
+python test_model.py -d higgs -m randomforest -f ONNXGPU --batch_size 10000 --query_size 10000
+python test_model.py -d higgs -m randomforest -f ONNXGPU --batch_size 1000000 --query_size 1000000
+# python test_model.py -d higgs -m randomforest -f HummingbirdTVMGPU --batch_size 100 --query_size 100
+# python test_model.py -d higgs -m randomforest -f HummingbirdTVMGPU --batch_size 10000 --query_size 10000
+# python test_model.py -d higgs -m randomforest -f HummingbirdTVMGPU --batch_size 1000000 --query_size 1000000
