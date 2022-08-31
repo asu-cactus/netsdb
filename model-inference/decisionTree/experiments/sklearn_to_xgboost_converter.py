56485daa708e8ff1c55e5c902d017c7b9d4a4d71
Mahidhar <mahidhar96@gmail.com>
2022-08-30 17:28:24 -0700

    converting models

--- model-inference/decisionTree/experiments/sklearn_to_xgboost_converter.py
+++ model-inference/decisionTree/experiments/sklearn_to_xgboost_converter.py
@@ -7,7 +7,8 @@ from model_helper import *
 models = os.listdir('models')
 for model_file in models:
     new_model = model_file.split('.')[0]+'.model'
-    if model_file.endswith(".pkl") and 'xgboost' in model_file and new_model not in models:
+    if (model_file.endswith(".pkl")) and ("torch" not in model_file) and ('xgboost' in model_file) and (new_model not in models):
+        print(model_file)
         clf = joblib.load(relative2abspath('models',model_file))
         clf.save_model(relative2abspath('models',model_file.split('.')[0]+'.model'))
-        print(model_file.split('.')[0]+'.model')
+        print(new_model)
