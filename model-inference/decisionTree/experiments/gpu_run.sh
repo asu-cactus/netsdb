ce07485ed2c94be49b79d7a34e44a81b9916e9c1
Mahidhar <mahidhar96@gmail.com>
2022-09-14 19:42:05 -0700

    removed memory erratic results

--- model-inference/decisionTree/experiments/gpu_run.sh
+++ model-inference/decisionTree/experiments/gpu_run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 DATASET="higgs"
-MODELS="lightgbm"
+MODELS="randomforest xgboost lightgbm"
 FRAMEWORKS="HummingbirdPytorchGPU HummingbirdTorchScriptGPU ONNXGPU HummingbirdTVMGPU NvidiaFILGPU"
 
 # Warmup
