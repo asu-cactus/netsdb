aa9f6a45774f0e3de55072b5d680f54ae48f8205
Mahidhar <mahidhar96@gmail.com>
2022-09-27 11:22:24 -0700

    added xgboost gpu testing

---
+++ model-inference/decisionTree/experiments/xgboost_gpu.sh
@@ -0,0 +1,7 @@
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 1 --query_size 1
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 10 --query_size 10
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 100 --query_size 100
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 1000 --query_size 1000
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 10000 --query_size 10000
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 100000 --query_size 100000
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 2200000 --query_size 2200000
