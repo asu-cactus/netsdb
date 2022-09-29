ea7e9d0c8dc9f1aefb190c029e73c04399b8a495
Mahidhar <mahidhar96@gmail.com>
2022-09-29 16:20:27 -0700

    epsilon training xgboost lightgbm

--- model-inference/decisionTree/experiments/epsilon_train.sh
+++ model-inference/decisionTree/experiments/epsilon_train.sh
@@ -1,8 +1,5 @@
 echo "started"
 
-python train_model.py -d epsilon -m randomforest -t 10
-python train_model.py -d epsilon -m randomforest -t 500
-python train_model.py -d epsilon -m randomforest -t 1600
 python train_model.py -d epsilon -m xgboost -t 10
 python train_model.py -d epsilon -m xgboost -t 500
 python train_model.py -d epsilon -m xgboost -t 1600
