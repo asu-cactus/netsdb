10b226d9891b6e0524e4f019a51b9ad005eb3c02
Mahidhar <mahidhar96@gmail.com>
2022-10-01 16:40:08 -0700

    xgboost testing higgs

--- model-inference/decisionTree/experiments/xgboost_gpu.sh
+++ model-inference/decisionTree/experiments/xgboost_gpu.sh
@@ -1,18 +1,18 @@
 echo "started"
 
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 100 --query_size 100 -t 10
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 1000 --query_size 1000 -t 10
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 10000 --query_size 10000 -t 10
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 100000 --query_size 100000 -t 10
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 2200000 --query_size 2200000 -t 10
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 100 --query_size 100 -t 500
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 1000 --query_size 1000 -t 500
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 10000 --query_size 10000 -t 500
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 100000 --query_size 100000 -t 500
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 2200000 --query_size 2200000 -t 500
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 100 --query_size 100 -t 1600
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 1000 --query_size 1000 -t 1600
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 10000 --query_size 10000 -t 1600
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 100000 --query_size 100000 -t 1600
-python test_model.py -d higgs -m randomforest -f XGBoostGPU --batch_size 2200000 --query_size 2200000 -t 1600
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 100 --query_size 100 -t 10
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 1000 --query_size 1000 -t 10
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 10000 --query_size 10000 -t 10
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 100000 --query_size 100000 -t 10
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 2200000 --query_size 2200000 -t 10
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 100 --query_size 100 -t 500
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 1000 --query_size 1000 -t 500
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 10000 --query_size 10000 -t 500
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 100000 --query_size 100000 -t 500
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 2200000 --query_size 2200000 -t 500
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 100 --query_size 100 -t 1600
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 1000 --query_size 1000 -t 1600
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 10000 --query_size 10000 -t 1600
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 100000 --query_size 100000 -t 1600
+python test_model.py -d higgs -m xgboost -f XGBoostGPU --batch_size 2200000 --query_size 2200000 -t 1600
 sudo shutdown now -h
