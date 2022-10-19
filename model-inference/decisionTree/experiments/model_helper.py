6b793f3e787201319369378cd441de28622be3f6
Mahidhar <mahidhar96@gmail.com>
2022-10-19 13:28:57 -0700

    debug query_data

--- model-inference/decisionTree/experiments/model_helper.py
+++ model-inference/decisionTree/experiments/model_helper.py
@@ -146,6 +146,7 @@ def run_inference(framework, features, input_size, query_size, predict, time_con
         for i in range(iterations):
             query_data = features[i*query_size:(i+1)*query_size]
             print(query_data)
+            query_data = query_data.todense()
             output = predict(query_data)
             results.extend(output)
 
