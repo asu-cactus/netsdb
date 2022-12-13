aa1215727407bd5a3bbee06252e3a36a7d9ae715
Mahidhar <mahidhar96@gmail.com>
2022-12-12 22:52:37 -0700

    changed features

--- model-inference/decisionTree/experiments/test_model.py
+++ model-inference/decisionTree/experiments/test_model.py
@@ -113,6 +113,8 @@ def load_data(config, time_consume):
     y_col = config[DATASET]["y_col"]
     x_col = list(test_data.columns)
     x_col.remove(y_col)
+    print("load_data===============")
+    print(config)
     if (config.get("dataset_type","dense")=="sparse"):
         features = test_data.todense()[x_col].to_numpy(dtype=np.float32)
     else:
